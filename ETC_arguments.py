a797d1e16e3d67605c2219257c30ddcc410029c0
Chaz Shapiro <chazshapiro@gmail.com>
2022-05-23 12:32:45 -0700

    Added positive/non-negative types to help with checking valid user input

--- ETC_arguments.py
+++ ETC_arguments.py
@@ -7,6 +7,26 @@ epilog = 'Example minimum argument set: \n./main.py G 500 510 SNR 10 -slit .5 -s
 parser = argparse.ArgumentParser(  # Make printed help text wider
   formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=40) ,description=help ,epilog=epilog)
 
+# Define some extra types to enforce positive values for arguments
+
+def posfloat(value): # require > 0
+    fvalue = float(value)
+    if fvalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive float" % value)
+    return fvalue
+
+def nonegfloat(value): # require >= 0
+    fvalue = float(value)
+    if fvalue < 0:
+        raise argparse.ArgumentTypeError("%s is an invalid non-negative float" % value)
+    return fvalue
+
+def posint(value): # require > 0
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive int" % value)
+    return ivalue
+
 SNRparam = parser.add_argument_group('SNR parameters')
 
 from ETC_config import channels
@@ -14,19 +34,19 @@ help = 'Spectrograph channel used for SNR'
 parser.add_argument('channel', type=str, choices=channels ,help=help)
 
 help = 'Min and max wavelength (nm) for SNR avg, e.g. "500 510". Will be rounded up to a whole number of bins'
-parser.add_argument('wrange', type=float ,nargs=2 ,help=help )
+parser.add_argument('wrange', type=posfloat ,nargs=2 ,help=help )
 
 help = 'Fix SNR or EXPTIME and calculate the other'
 parser.add_argument('ETCmode', type=str, choices=['SNR','EXPTIME'], help=help)
 
 help = 'Value of the fixed parameter: SNR (dimensionless) or EXPTIME (s)'
-parser.add_argument('ETCfixed', type=float, help=help)
+parser.add_argument('ETCfixed', type=posfloat, help=help)
 
 help = 'On-chip binning along dispersion (D) and spatial (S) axes'
-parser.add_argument('-binning', type=int, nargs=2, default=[1,1], metavar=('BIN_D','BIN_S') ,help=help)
+parser.add_argument('-binning', type=posint, nargs=2, default=[1,1], metavar=('BIN_D','BIN_S') ,help=help)
 
 help = 'Number of spatial pixels on either side of profile center to use for SNR. If none, fit spatial profile'
-parser.add_argument('-SNR_pix', type=int, default=None, help=help)
+parser.add_argument('-SNR_pix', type=posint, default=None, help=help)
 
 help = 'Only use flux from the center slit, not side slices'
 parser.add_argument('-noslicer', action='store_true', help=help)
@@ -40,10 +60,10 @@ parser.add_argument('-plotdiag', action='store_true', help=help)
 obsparam = parser.add_argument_group('REQUIRED Observation conditions')
 
 help = 'Slit width (arcsec)'
-obsparam.add_argument('-slit', type=float, required=True, help=help)
+obsparam.add_argument('-slit', type=posfloat, required=True, help=help)
 
 help = 'Seeing FWHM (arcsec) defined at pivot wavelength (nm)'
-obsparam.add_argument('-seeing', type=float, nargs=2, metavar=('SEEING','PIVOT'), required=True, help=help)
+obsparam.add_argument('-seeing', type=posfloat, nargs=2, metavar=('SEEING','PIVOT'), required=True, help=help)
 
 help = 'Airmass (dimensionless)'
 obsparam.add_argument('-airmass', type=float, required=True, help=help)
@@ -69,10 +89,10 @@ help = 'Source template category and filename; REQUIRED with -srcmodel template'
 sourceparam_add.add_argument('-srctemp', type=str, nargs=2, metavar=('CAT','FILE'), help=help)
 
 help = 'Blackbody temperature (K); REQUIRED with -srcmodel blackbody'
-sourceparam_add.add_argument('-tempK', type=float, help=help)
+sourceparam_add.add_argument('-tempK', type=posfloat, help=help)
 
 help = 'Redshift'
-sourceparam_add.add_argument('-z', type=float, default=0., help=help)
+sourceparam_add.add_argument('-z', type=nonegfloat, default=0., help=help)
 
 help = 'Selective Extinction E(B-V); default=0'
 sourceparam_add.add_argument('-E_BV', type=float, default=0., help=help)
@@ -124,7 +144,7 @@ def check_inputs_add_units(args):
 
 	# Check wavelength range is (min, max) and within specified channel
 	from ETC_config import channelRange
-	if args.wrange[0] >= args.wrange[1]: raise ValueError("Wavelength range must be in form [min, max]")
-	if args.wrange[0] < channelRange[args.channel][0]: raise ValueError("Wavelength range not in channel")
-	if args.wrange[1] > channelRange[args.channel][1]: raise ValueError("Wavelength range not in channel")
+	if args.wrange[0] >= args.wrange[1]: parser.error("Wavelength range must be in form [min, max]")
+	if args.wrange[0] < channelRange[args.channel][0]: parser.error("Wavelength range not in channel %s"%args.channel)
+	if args.wrange[1] > channelRange[args.channel][1]: parser.error("Wavelength range not in channel %s"%args.channel)
 
